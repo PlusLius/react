50db11a4365080d4cf3f8aad33d213c9170d7c08
JiaHao <408709581@qq.com>
2024-02-06 12:09:44 +0800

    Update getEventTarget.js

--- packages/react-dom/src/events/getEventTarget.js
+++ packages/react-dom/src/events/getEventTarget.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+// 获取事件对象
 import {TEXT_NODE} from '../shared/HTMLNodeType';
 
 /**
